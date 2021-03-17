bc1a01bae0519ce46ab62e9043f40fae3eb308e3
agustin-recoba <agustin.recoba@fing.edu.uy>
2021-03-17 12:01:27 -0300

    awdwd

---
+++ practicos/prac2/ej2.cpp
@@ -0,0 +1,12 @@
+typedef unsigned int uint;
+
+bool esPolindromeAux(char *string, uint n, uint deep) {
+    if (deep >= n/2 - 1) 
+        return true;
+    else
+        return string[1+deep] == string[n-deep] && esPolindromeAux(string, n, deep + 1);
+}
+
+bool esPolindrome(char *string, uint n){
+    return esPolindromeAux(string, n, 0);
+}
