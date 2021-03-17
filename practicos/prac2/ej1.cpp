4b43a56a453a7a683867ea95e0855d27ee5caefc
agustin-recoba <agustin.recoba@fing.edu.uy>
2021-03-17 11:22:15 -0300

    awd

--- practicos/prac2/ej1.cpp
+++ practicos/prac2/ej1.cpp
@@ -2,7 +2,15 @@ typedef unsigned int uint;
 
 void insertarOrdenado(float * A, uint n, float e)
 {
-    
+    if (n == 0) {
+        A[n + 1] = e;
+    } else if (A[n] < e) {
+        A[n+1] = e;
+        n++;
+    } else {
+        A[n+1] = A[n];
+        insertarOrdenado(A, n-1, e);
+    }
 }
 
 void ordenar(float * A, uint n)
