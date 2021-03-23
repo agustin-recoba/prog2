4f568695b1814ea7e3415c225b69a644c5a72790
agustin-recoba <agustin.recoba@fing.edu.uy>
2021-03-23 16:23:22 -0300

    6

--- practicos/prac3/ej5.cpp
+++ practicos/prac3/ej5.cpp
@@ -6,5 +6,56 @@ struct  nodo_doble {
     nodo_doble *sig;
     nodo_doble *ant;
 };
+
 typedef  nodo_doble *lista;
+typedef unsigned int nat;
+
+bool isDefined (lista l, nat i) {
+    int j = 0;
+    while (l != NULL && j <= i) {
+        l = l->sig;
+        j++;
+    }
+    return l != NULL;
+}
+
+void insert(lista &l1, int x, nat i) {
+    int j = 0;
+    lista l = l1;
+    while (l != NULL && j <= i) {
+        l = l->sig;
+        j++;
+    }
+    lista nueva = new nodo_doble;
+    nueva->elem = x;
+    nueva->sig = l;
+    nueva->ant = l->ant;
+    l->ant->sig = nueva;
+    l->ant = nueva;
+}
+
+int element(lista &l1, nat i) {
+    int j = 0;
+    lista l = l1;
+    while (l != NULL && j <= i) {
+        l = l->sig;
+        j++;
+    }
+    return l->elem;
+}
+
+void remove(lista &l1, nat p) {
+    if (isDefined(l1, p)) {
+        lista l = l1;
+        int j = 0;
+        while (l != NULL && j <= p) {
+            l = l->sig;
+            j++;
+        }
+        lista aux = l;
+        l->ant = l->sig;
+        l->sig = aux->ant;
+        delete aux;
+    }
+}
 
