be1704cb8d5912278a149bd199b1e4ce3be7c166
agustin-recoba <agustin.recoba@fing.edu.uy>
2021-04-18 20:31:53 -0300

    bien

--- tarea3/src/binario.cpp
+++ tarea3/src/binario.cpp
@@ -12,8 +12,84 @@ struct _rep_binario {
 };
 
 TBinario crearBinario() {
-    TBinario nuevo = new _rep_binario;
-    nuevo->elem = NULL;
-    nuevo->izq = nuevo->der = NULL;
-    return nuevo;
+    return NULL;
+}
+
+TBinario insertarEnBinario(TInfo i, TBinario b) {
+    if(b == NULL) {
+        b = new _rep_binario;
+        b->elem = copiaInfo(i);
+        b->izq = b->der = NULL;
+    } else if (natInfo(i) < natInfo(b->elem))
+        b->izq = insertarEnBinario(i, b->izq);
+    else if(natInfo(i) > natInfo(b->elem))
+        b->der = insertarEnBinario(i, b->der);
+    return b;
+}
+
+TInfo mayor(TBinario b) {
+    while(b->der != NULL)
+        b = b->der;
+    return b->elem;
+}
+
+TBinario removerMayor(TBinario b){
+    
+    return b;
+}
+
+TBinario removerDeBinario(nat elem, TBinario b) {
+    return b;
+}
+
+void liberarBinario(TBinario b){
+
+}
+
+bool esVacioBinario(TBinario b){
+    return true;
+}
+
+bool esAvl(TBinario b){
+    return true;
+}
+
+TInfo raiz(TBinario b) {
+    return b->elem;
+}
+
+TBinario izquierdo(TBinario b) {
+    return b->izq;
+}
+
+TBinario derecho(TBinario b) {
+    return b->der;
+}
+
+TBinario buscarSubarbol(nat elem, TBinario b) {
+    return b;
+}
+
+nat alturaBinario(TBinario b){
+    return 1;
+}
+
+nat cantidadBinario(TBinario b) {
+    return 2;
+}
+
+double sumaUltimosPositivos(nat i, TBinario b){
+    return 0.0;
+}
+
+TCadena linealizacion(TBinario b) {
+    return NULL;
+}
+
+TBinario menores(double cota, TBinario b) {
+    return b;
+}
+
+void imprimirBinario(TBinario b) {
+
 }
