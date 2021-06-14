7c8f781325193733075330b6a59db98ef1623486
agustin-recoba <agustin.recoba@fing.edu.uy>
2021-06-14 00:19:33 -0300

    terminado

--- tarea6/src/colaDePrioridad.cpp
+++ tarea6/src/colaDePrioridad.cpp
@@ -7,24 +7,24 @@
 #include <stdlib.h>
 #include <math.h>
 
-typedef double* prioridades;
-
 struct _rep_colaDePrioridad {
     nat *heap;
-    prioridades *elementos;
+    double *prioridades;
+    nat *posiciones;
     nat tamHeap, espUsadoHeap;
 };
 
 TColaDePrioridad crearCP(nat N) {
-
     TColaDePrioridad col = new _rep_colaDePrioridad;
     col->espUsadoHeap = 0;
     col->tamHeap = N;
     col->heap = new nat[N+1];
-    col->elementos = new prioridades[N];
-    for (nat i = 0; i < N; i++)
-        col->elementos[i] = NULL;
-
+    col->prioridades = new double[N+1];
+    col->posiciones = new nat[N+1];
+    for (nat i = 0; i <= N; i++) {
+        col->prioridades[i] = -1;
+        col->posiciones[i] = 0;
+    }
     return col;
 }
 
@@ -38,20 +38,25 @@ static void swap(nat &i1, nat &i2) {
     i1 = aux;
 }
 
+/*static void imprimirHeap(TColaDePrioridad cp) {
+    for (nat i = 0; i <= cp->tamHeap; i++)
+        printf("%d", cp->heap[i]);
+    printf("\n");
+}*/
+
 TColaDePrioridad insertarEnCP(nat elem, double valor, TColaDePrioridad cp) {
-    cp->elementos[elem-1] = new double;
-    *(cp->elementos[elem-1]) = valor;
-
+    cp->prioridades[elem] = valor;
     cp->espUsadoHeap++;
-    nat espLibre = cp->espUsadoHeap;
+    cp->posiciones[elem] = cp->espUsadoHeap;
+    cp->heap[cp->espUsadoHeap] = elem;
 
-    cp->heap[espLibre] = elem;
+    nat i = cp->espUsadoHeap;
 
-    while (espLibre/2 != 0 && prioridad(cp->heap[espLibre/2], cp) > prioridad(cp->heap[espLibre], cp)) {
-        swap(cp->heap[espLibre/2], cp->heap[espLibre]);
-        espLibre = espLibre/2;
+    while (i/2 != 0 && prioridad(cp->heap[i/2], cp) > prioridad(cp->heap[i], cp)) {
+        swap(cp->heap[i/2], cp->heap[i]);
+        swap(cp->posiciones[cp->heap[i/2]], cp->posiciones[cp->heap[i]]);
+        i = i/2;
     }
-
     return cp;
 }
 
@@ -64,61 +69,100 @@ nat prioritario(TColaDePrioridad cp) {
 }
 
 TColaDePrioridad eliminarPrioritario(TColaDePrioridad cp) {
-    delete cp->elementos[cp->heap[1]];
-    cp->elementos[cp->heap[1]] = NULL;
+    cp->prioridades[cp->heap[1]] = -1;
+    cp->posiciones[cp->heap[1]] = 0;
+    cp->heap[1] = 0;
 
-    nat aColocar = cp->espUsadoHeap;
+    nat aColocar = cp->heap[cp->espUsadoHeap];
     cp->espUsadoHeap--;
     nat i = 1;
     bool colocado = false;
-
-    while (i*2 <= cp->espUsadoHeap && i*2+1 <= cp->espUsadoHeap && !colocado) {
-        nat min = prioridad(cp->heap[i*2], cp) < prioridad(cp->heap[i*2+1], cp) ? i*2 : i*2 + 1;
-        if (prioridad(aColocar, cp) <= prioridad(cp->heap[min], cp)) {
+    while (i*2+1 <= cp->espUsadoHeap && !colocado) {
+        nat hijoMin = prioridad(cp->heap[i*2], cp) >= prioridad(cp->heap[i*2+1], cp) ? i*2 : i*2+1;
+        if (prioridad(aColocar, cp) <= prioridad(cp->heap[hijoMin], cp)) {
             cp->heap[i] = aColocar;
+            cp->posiciones[aColocar] = i;
             colocado = true;
         } else {
-            cp->heap[i] = cp->heap[min];
-            i = min;
+            cp->heap[i] = cp->heap[hijoMin];
+            cp->posiciones[cp->heap[hijoMin]] = i;
+            i = hijoMin;
         }
     }
-    if (!colocado) {
+    if (!colocado && i*2 == cp->espUsadoHeap) {
+
+        if (prioridad(aColocar, cp) <= prioridad(cp->heap[i*2], cp)) {
+            cp->heap[i] = aColocar;
+            cp->posiciones[aColocar] = i;
+        } else {
+            cp->heap[i] = cp->heap[i*2];
+            cp->posiciones[cp->heap[i*2]] = i;
+            cp->heap[i*2] = aColocar;
+            cp->posiciones[aColocar] = i*2;
+        }
+    } else if (!colocado) {
         cp->heap[i] = aColocar;
-    } 
+        cp->posiciones[aColocar] = i;
+    }
+
+    
     return cp;
 }
 
 bool estaEnCP(nat elem, TColaDePrioridad cp) {
-    return cp->elementos[elem] != NULL;
+    return cp->prioridades[elem] != -1;
 }
 
 double prioridad(nat elem, TColaDePrioridad cp) {
-    return *cp->elementos[elem];
+    return cp->prioridades[elem];
 }
 
-
-/*
-  Modifica el valor de la propiedad asociada a 'elem'; pasa a ser 'valor'.
-  Precondición: estaEnCp(elem, cp).
-  Devuelve 'cp'.
-  El tiempo de ejecución en el peor caso es O(log N), siendo 'N' el parámetro
-  pasado en crearCP.
- */
 TColaDePrioridad actualizarEnCP(nat elem, double valor, TColaDePrioridad cp) {
-    *(cp->elementos[elem]) = valor;
+    bool aumentaPrioridad = valor > cp->prioridades[elem];
+    cp->prioridades[elem] = valor;
+    nat i = cp->posiciones[elem];
+
+    if (aumentaPrioridad) {
+        bool colocado = false;
+        while(i*2 <= cp->espUsadoHeap && i*2+1 <= cp->espUsadoHeap && !colocado) {
+            nat min = cp->prioridades[i*2] < cp->prioridades[i-2+1] ? i*2 : i*2+1;
+            if (prioridad(elem, cp) <= prioridad(cp->heap[min], cp)) {
+                cp->heap[i] = elem;
+                cp->posiciones[elem] = i;
+                colocado = true;
+            } else {
+                cp->heap[i] = cp->heap[min];
+                cp->posiciones[cp->heap[min]] = i;
+                i = min;
+            }
+        }
+        if (!colocado && i*2 == cp->espUsadoHeap) {
+            if (prioridad(elem, cp) <= prioridad(cp->heap[i*2], cp)) {
+                cp->heap[i] = elem;
+                cp->posiciones[elem] = i;
+            } else {
+                cp->heap[i] = cp->heap[i*2];
+                cp->posiciones[cp->heap[i*2]] = i;
+                cp->heap[i*2] = elem;
+                cp->posiciones[elem] = i*2;
+            }
+        } else if (!colocado) {
+            cp->heap[i] = elem;
+            cp->posiciones[elem] = i;
+        }
+    } else {
+        while (i/2 != 0 && prioridad(cp->heap[i/2], cp) > prioridad(cp->heap[i], cp)) {
+            swap(cp->heap[i/2], cp->heap[i]);
+            swap(cp->posiciones[cp->heap[i/2]], cp->posiciones[cp->heap[i]]);
+            i = i/2;
+        }
+    }
     return cp;
 }
 
-
-/*
-  Libera la menoria asignada a 'cp'.
-  El tiempo de ejecución en el peor caso es O(N), siendo 'N' el parámetro
-  pasado en crearCP.
- */
 void liberarCP(TColaDePrioridad cp) {
-    for (nat i = 1; i <= cp->espUsadoHeap; i++)
-        delete cp->elementos[i];
-    delete[] cp->elementos;
+    delete[] cp->posiciones;
+    delete[] cp->prioridades;
     delete[] cp->heap;
     delete cp;
 }
